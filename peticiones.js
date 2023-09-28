5456e5cee6d78ac51aff71d1cc3fd991d86e5674
JesusCCGG <gchuysin@gmail.com>
2023-09-28 13:57:44 -0600

    inicializacion del repositorio

---
+++ peticiones.js
@@ -0,0 +1,12 @@
+function objeto(){
+    let usuario={
+    nombre:"carlos",
+    correo:"carlos@dominio.mx",
+    carrera:{
+        nombre:"Telematica",
+        semestre:10
+    }
+}
+
+console.log(usuario);
+}
